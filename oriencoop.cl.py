bc26c247c19ac2d6155663f0f103953007f25f5a
roman-bs <375445927023@mail.ru>
2022-10-20 12:00:26 +0300

    add RocketData

--- oriencoop.cl.py
+++ oriencoop.cl.py
@@ -2,22 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 from geopy.geocoders import Nominatim
 import json
+import re
 
-
-url = "https://oriencoop.cl/sucursales.htm"
-
-
-headers = {
-    "Accept": "*/*",
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.167 YaBrowser/22.7.5.934 (beta) Yowser/2.5 Safari/537.36"
-}
-
-req = requests.get(url, headers=headers)
-src = req.text
-
-
-with open("index.html", 'w') as file:
-    file.write(src)
+# url = "https://oriencoop.cl/sucursales.htm"
+#
+#
+# headers = {
+#     "Accept": "*/*",
+#     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.167 YaBrowser/22.7.5.934 (beta) Yowser/2.5 Safari/537.36"
+# }
+#
+# req = requests.get(url, headers=headers)
+# src = req.text
+#
+#
+# with open("index.html", 'w') as file:
+#     file.write(src)
 
 
 with open("index.html") as file:
@@ -28,6 +28,12 @@ soup = BeautifulSoup(src, "lxml")
 data = soup.find(class_="s-dato").find_all('span') # парсинг выбранного тега
 data_name = soup.find(class_="s-dato").find('h3')
 
+
+
+m = re.search(r'2d=(\d+\.\d+),(\d+\.\d+)', 'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6689.174124950521!2d-71.625896!3d-33.041008!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9689e12eaa95f955%3A0xcf70834bd8c5ef8c!2sEsmeralda+940%2C+Valpara%C3%ADso%2C+Regi%C3%B3n+de+Valpara%C3%ADso%2C+Chile!5e0!3m2!1ses!2sus!4v1495663213494')
+print(m.groups())
+
+z =
 
 address = "address"
 name = "name"
